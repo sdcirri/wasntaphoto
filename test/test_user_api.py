3fdc58f30050bf02cc9cfb9eda71015cd4330419
sdcirri <sebadeod@proton.me>
2026-03-13 19:47:11 +0100

    following mechanics tests

--- test/test_user_api.py
+++ test/test_user_api.py
@@ -1,30 +1,85 @@
+import asyncio
 import pytest
 
 
 @pytest.mark.asyncio
 async def test_user_api(client, user_factory):
-    await user_factory('alice', 'H@xx0r.2026')
-    await user_factory('bob', 'T0P.S3cr3t!')
+    alice_user, bob_user = await asyncio.gather(
+        user_factory('alice', 'H@xx0r.2026'),
+        user_factory('bob', 'T0P.S3cr3t!')
+    )
 
     login = await client.post('/session/', json={'username': 'alice', 'password': 'H@xx0r.2026'})
-    token = login.json()
+    headers = {'Authorization': f'Bearer {login.json()}'}
 
-    alice_info = await client.get('/users/1', headers={'Authorization': f'Bearer {token}'})
+    alice_info = await client.get(f'/users/{alice_user.user_id}', headers=headers)
     assert alice_info.status_code == 200
     assert alice_info.json()['username'] == 'alice'
-    assert alice_info.json()['user_id'] == 1
+    assert alice_info.json()['user_id'] == alice_user.user_id
 
-    me_info = await client.get('/users/me', headers={'Authorization': f'Bearer {token}'})
+    me_info = await client.get('/users/me', headers=headers)
     assert me_info.status_code == 200
     assert me_info.json()['username'] == 'alice'
-    assert me_info.json()['user_id'] == 1
+    assert me_info.json()['user_id'] == alice_user.user_id
 
-    bob_info = await client.get('/users/2', headers={'Authorization': f'Bearer {token}'})
+    bob_info = await client.get(f'/users/{bob_user.user_id}', headers=headers)
     assert bob_info.status_code == 200
     assert bob_info.json()['username'] == 'bob'
-    assert bob_info.json()['user_id'] == 2
+    assert bob_info.json()['user_id'] == bob_user.user_id
 
-    username_change = await client.put('/users/me/username', json='@lix', headers={'Authorization': f'Bearer {token}'})
+    username_change = await client.put('/users/me/username', json='@lix', headers=headers)
     assert username_change.status_code == 204
-    me_info = await client.get('/users/me', headers={'Authorization': f'Bearer {token}'})
+    me_info = await client.get('/users/me', headers=headers)
     assert me_info.json()['username'] == '@lix'
+
+
+@pytest.mark.asyncio
+async def test_following_mechanics(client, user_factory):
+    alice_user, bob_user, annoying_user = await asyncio.gather(
+        user_factory('alice', 'H@xx0r.2026'),
+        user_factory('bob', 'T0P.S3cr3t!'),
+        user_factory('annoying', 'I@mAnn0ying!')
+    )
+    alice_login, bob_login, annoying_login = await asyncio.gather(
+        client.post('/session/', json={'username': 'alice', 'password': 'H@xx0r.2026'}),
+        client.post('/session/', json={'username': 'bob', 'password': 'T0P.S3cr3t!'}),
+        client.post('/session/', json={'username': 'annoying', 'password': 'I@mAnn0ying!'})
+    )
+    alice_headers, bob_headers, annoying_headers = (
+        {'Authorization': f'Bearer {alice_login.json()}'},
+        {'Authorization': f'Bearer {bob_login.json()}'},
+        {'Authorization': f'Bearer {annoying_login.json()}'}
+    )
+
+    following = await client.get('/users/me/following', headers=alice_headers)
+    assert following.status_code == 200
+    assert not following.json()
+
+    await client.post(f'/users/me/following/{bob_user.user_id}', headers=alice_headers)
+    following, followers = await asyncio.gather(
+        client.get('/users/me/following', headers=alice_headers),
+        client.get('/users/me/followers', headers=bob_headers)
+    )
+    assert bob_user.user_id in following.json()
+    assert alice_user.user_id in followers.json()
+
+    unfollow = await client.delete(f'/users/me/following/{bob_user.user_id}', headers=alice_headers)
+    assert unfollow.status_code == 204
+    following = await client.get('/users/me/following', headers=alice_headers)
+    assert bob_user.user_id not in following.json()
+
+    await client.post(f'/users/me/following/{annoying_user.user_id}', headers=alice_headers)
+    await client.post(f'/users/me/blocked/{annoying_user.user_id}', headers=alice_headers)
+    blocked = await client.get('/users/me/blocked', headers=alice_headers)
+    assert blocked.status_code == 200
+    assert annoying_user.user_id in blocked.json()
+    following = await client.get('/users/me/following', headers=alice_headers)
+    assert annoying_user.user_id not in following.json()
+
+    unblock = await client.delete(f'/users/me/blocked/{annoying_user.user_id}', headers=alice_headers)
+    assert unblock.status_code == 204
+    blocked = await client.get('/users/me/blocked', headers=alice_headers)
+    assert annoying_user.user_id not in blocked.json()
+
+    bad_follow = await client.post(f'/users/me/following/{alice_user.user_id}', headers=alice_headers)
+    assert bad_follow.status_code == 400
