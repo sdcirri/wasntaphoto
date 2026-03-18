e7f237ebb33151eebd55190eab779cf54e59e8b8
sdcirri <sebadeod@proton.me>
2026-03-18 19:02:54 +0100

    added fake origin to CORS, frozen deps

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI, Request
 
-from exceptions import AppError
 from api.login_api import login_router
 from api.feed_api import feed_router
 from api.user_api import user_router
+from exceptions import AppError
 
 
 app = FastAPI(title='WASNTAPhoto Backend', openapi_url=None, docs_url=None, redoc_url=None)
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=['*'],
+    #allow_origins=['https://wasntaphoto.com/'],
     allow_credentials=True,
     allow_methods=['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
     allow_headers=['Accept', 'Authorization', 'Content-Type'],
